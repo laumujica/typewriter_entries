194af1e13a1e945196e26a6a81f582d61dc6ed57
lauraamujica <lauraamujica@gmail.com>
2023-08-12 00:13:44 -0300

    fixing modal (done) + fixing 1st entry (due)

--- script.js
+++ script.js
@@ -1,123 +1,125 @@
-const typedText = document.querySelector(".typed-text");
-const entryTitle = document.querySelector(".entry-title");
-const savedList = document.querySelector(".saved-list");
-const modal = document.querySelector(".modal-overlay");
-const modalTitle = document.querySelector(".modal-title");
-const modalContent = document.querySelector(".modal-content");
-const modalClose = document.querySelector(".modal-close");
-const saveButton = document.querySelector(".save-button");
-
-// Initialize Firebase!
-const firebaseConfig = {
-  apiKey: "AIzaSyCX6yqmOzw34lvST1DjWjCV3D0yUxFJHbg",
-  authDomain: "typewriter-entries.firebaseapp.com",
-  databaseURL: "https://typewriter-entries-default-rtdb.firebaseio.com",
-  projectId: "typewriter-entries",
-  storageBucket: "typewriter-entries.appspot.com",
-  messagingSenderId: "658456453344",
-  appId: "1:658456453344:web:6040ebce270daf7248f085",
-  measurementId: "G-BZTXEJPM9M",
-};
-
-firebase.initializeApp(firebaseConfig);
-
-// Reference to your entries in the database
-var entriesRef = firebase.database().ref("entries");
-
-// Fetch the entries
-entriesRef.on("value", function (snapshot) {
-  var entries = snapshot.val();
-  displayEntries(entries);
-});
-
 document.addEventListener("DOMContentLoaded", function () {
-  // Call displayEntries here
-  displayEntries();
-});
+  const typedText = document.querySelector(".typed-text");
+  const entryTitle = document.querySelector(".entry-title");
+  const savedList = document.querySelector(".saved-list");
+  const modal = document.querySelector(".modal-overlay");
+  const modalTitle = document.querySelector(".modal-title");
+  const modalContent = document.querySelector(".modal-content");
+  const modalClose = document.querySelector(".modal-close");
+  const saveButton = document.querySelector(".save-button");
 
-// Establecer una referencia a la base de datos de Firebase
-const database = firebase.database();
+  // Initialize Firebase!
+  const firebaseConfig = {
+    apiKey: "AIzaSyCX6yqmOzw34lvST1DjWjCV3D0yUxFJHbg",
+    authDomain: "typewriter-entries.firebaseapp.com",
+    databaseURL: "https://typewriter-entries-default-rtdb.firebaseio.com",
+    projectId: "typewriter-entries",
+    storageBucket: "typewriter-entries.appspot.com",
+    messagingSenderId: "658456453344",
+    appId: "1:658456453344:web:6040ebce270daf7248f085",
+    measurementId: "G-BZTXEJPM9M",
+  };
 
-/* No sé si esto romperá algo */
-function displayEntries(entries) {
-  var entriesList = document.querySelector(".entries-list");
-  entriesList.innerHTML = ""; // Clear existing entries
+  firebase.initializeApp(firebaseConfig);
 
-  for (var key in entries) {
-    var entry = entries[key];
-    var entryItem = document.createElement("li");
-    entryItem.textContent = entry.title;
-    entryItem.addEventListener("click", function () {
-      // Display the entry details in the modal
-      openModal(entry.title, entry.content);
-    });
-    entriesList.appendChild(entryItem);
-  }
-}
+  // Reference to your entries in the database
+  var entriesRef = firebase.database().ref("entries");
 
-/* Acá empieza todo creo */
-entryTitle.addEventListener("click", () => {
-  const titleElement = document.getElementById("new-title");
-  if (titleElement.textContent === "Nueva entrada") {
-    titleElement.textContent = "";
-  }
-});
+  // Fetch the entries
+  entriesRef.on("value", function (snapshot) {
+    var entries = snapshot.val();
+    displayEntries(entries);
+  });
 
-typedText.addEventListener("click", () => {
-  const typedText = document.getElementById("new-text");
-  if (typedText.textContent === "Escriba su texto aquí") {
-    typedText.textContent = "";
-  }
-});
+  document.addEventListener("DOMContentLoaded", function () {
+    // Call displayEntries here
+    displayEntries();
+  });
 
-saveButton.addEventListener("click", () => {
-  const content = typedText.textContent;
-  const title = entryTitle.textContent;
+  // Establecer una referencia a la base de datos de Firebase
+  const database = firebase.database();
 
-  if (content.trim() !== "" && title.trim() !== "" && title !== "Título") {
-    const id = `escrito_${Date.now()}`;
+  /* No sé si esto romperá algo */
+  function displayEntries(entries) {
+    var entriesList = document.querySelector(".saved-list");
+    entriesList.innerHTML = ""; // Clear existing entries
 
-    const entryData = {
-      title: title,
-      content: content,
-    };
-
-    database
-      .ref("entradas/" + id)
-      .set(entryData)
-      .then(() => {
-        const listItem = document.createElement("li");
-        const link = document.createElement("a");
-
-        link.textContent = title;
-        link.href = "#" + id;
-        link.addEventListener("click", (event) => {
-          event.preventDefault();
-          modalTitle.textContent = title;
-          modalContent.textContent = content;
-          modal.style.display = "block";
-        });
-
-        listItem.appendChild(link);
-        savedList.appendChild(listItem);
-
-        typedText.textContent = "Escriba su texto aquí";
-        entryTitle.textContent = "Nueva entrada";
-      })
-      .catch((error) => {
-        console.error("Error al guardar la entrada: ", error);
+    for (var key in entries) {
+      var entry = entries[key];
+      var entryItem = document.createElement("li");
+      entryItem.textContent = entry.title;
+      entryItem.addEventListener("click", function () {
+        // Display the entry details in the modal
+        openModal(entry.title, entry.content);
       });
+      entriesList.appendChild(entryItem);
+    }
   }
-});
 
-modalClose.addEventListener("click", () => {
-  modal.style.display = "none";
-});
+  /* Acá empieza todo creo */
+  entryTitle.addEventListener("click", () => {
+    const titleElement = document.getElementById("new-title");
+    if (titleElement.textContent === "Nueva entrada") {
+      titleElement.textContent = "";
+    }
+  });
 
-modal.addEventListener("click", (event) => {
-  if (event.target === modal) {
+  typedText.addEventListener("click", () => {
+    const typedText = document.getElementById("new-text");
+    if (typedText.textContent === "Escriba su texto aquí") {
+      typedText.textContent = "";
+    }
+  });
+
+  saveButton.addEventListener("click", () => {
+    const content = typedText.textContent;
+    const title = entryTitle.textContent;
+
+    if (content.trim() !== "" && title.trim() !== "" && title !== "Título") {
+      const id = `escrito_${Date.now()}`;
+
+      const entryData = {
+        title: title,
+        content: content,
+      };
+
+      database
+        .ref("entradas/" + id)
+        .set(entryData)
+        .then(() => {
+          const listItem = document.createElement("li");
+          const link = document.createElement("a");
+
+          link.textContent = title;
+          link.href = "#" + id;
+          link.addEventListener("click", (event) => {
+            event.preventDefault();
+            modalTitle.textContent = title;
+            modalContent.textContent = content;
+            modal.style.display = "block";
+          });
+
+          listItem.appendChild(link);
+          savedList.appendChild(listItem);
+
+          typedText.textContent = "Escriba su texto aquí";
+          entryTitle.textContent = "Nueva entrada";
+        })
+        .catch((error) => {
+          console.error("Error al guardar la entrada: ", error);
+        });
+    }
+  });
+
+  modalClose.addEventListener("click", () => {
     modal.style.display = "none";
-  }
-});
+  });
 
-// ... (otros eventos y código posterior)
+  modal.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+
+  // ... (otros eventos y código posterior)
+});
